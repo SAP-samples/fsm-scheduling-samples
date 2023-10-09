b86941ba818ee5a080345246f41ad1d342d59fc6
Davo00 <davit.melkonyan@sap.com>
2023-10-09 14:48:13 +0200

    adjusted the query.service.ts to the modern response shape of the api; fixed lots of issues reported by tslint

--- workbench/frontend/src/app/common/services/query.service.ts
+++ workbench/frontend/src/app/common/services/query.service.ts
@@ -1,10 +1,10 @@
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, mergeMap, tap, withLatestFrom } from 'rxjs/operators';
+import { map, mergeMap, tap } from 'rxjs/operators';
 import { ConfigService } from './config.service';
 import { CLIENT_IDENTIFIER } from '../contants';
-import { GlobalContext, AuthService } from './auth.service';
-import { combineLatest } from 'rxjs';
+import { AuthService, GlobalContext } from './auth.service';
+import { combineLatest, Observable, ObservableInput } from 'rxjs';
 
 type PersonObj = { id: string, firstName: string, lastName: string, skills: { name: string, start: string, end: string }[] };
 type ActivityObj = { id: string, subject: string; startDateTime: string, earliestStartDateTime: string, dueDateTime: string, code: string };
@@ -14,6 +14,10 @@ type SkillObj = { id: string, tag: string, person: string, endDate: string, star
 type InternalCache = {
   resource: Map<string, PersonObj>;
   activity: Map<string, ActivityObj>;
+};
+
+interface QueryResponse<T> {
+  data: T[];
 }
 
 @Injectable({
@@ -21,19 +25,43 @@ type InternalCache = {
 })
 export class QueryService {
 
+
+  // tslint:disable-next-line:variable-name
   private __cache: InternalCache = {
     resource: new Map<string, PersonObj>(),
     activity: new Map<string, ActivityObj>()
+  };
+
+  public static toLastChangedDateStringFormat = (lastChanged: number): string => {
+    // ¯\_(ツ)_/¯ queryApi only supports dateTime on [lastChanged]
+    // but only formats supported are:
+    // [yyyy-MM-ddThh:mm:ssZ]
+    // [yyyy-MM-dd hh:mm:ss.SSS]
+    // ISO:
+    // [yyyy-MM-ddThh:mm:ss.SSSZ] is NOT supported
+    return new Date(lastChanged)
+      .toISOString()
+      .replace('T', ' ')
+      .replace('Z', '');
   }
 
-  private getCache<T extends Map<string, any>>(key: keyof InternalCache) {
+  public static trimMilliseconds = (dateString: string) => {
+    // ¯\_(ツ)_/¯ queryApi and dates ...
+    // ISO: [yyyy-MM-ddThh:mm:ss.SSSZ] is NOT supported
+    // but
+    // [yyyy-MM-ddThh:mm:ssZ]
+    return new Date(new Date(dateString).setMilliseconds(0)).toISOString().replace('.000', '');
+  }
+
+
+  private getCache<T extends Map<string, any>>(key: keyof InternalCache): T {
     return this.__cache[key] as T;
   }
 
-  private getHeaders(ctx: GlobalContext) {
+  private getHeaders(ctx: GlobalContext): HttpHeaders {
     return new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `${ctx.authToken}`,
+      Authorization: `${ctx.authToken}`,
       'x-cloud-host': `${ctx.cloudHost}`,
       'x-account-name': `${ctx.account}`,
       'x-account-id': `${ctx.accountId}`,
@@ -43,71 +71,88 @@ export class QueryService {
       'x-user-id': `${ctx.userId}`,
       'x-client-id': CLIENT_IDENTIFIER,
       'x-client-version': '0.0.0',
-      'x-request-id': `${Date.now()}`,
-    })
+      'x-request-id': `${Date.now()}`
+    });
   }
 
   constructor(
     private config: ConfigService,
     private auth: AuthService,
-    private http: HttpClient,
-  ) { }
+    private http: HttpClient
+  ) {
+  }
 
-  private addToCache(key: keyof InternalCache, list: Partial<PersonObj>[] | Partial<ActivityObj>[]) {
+  private addToCache(key: keyof InternalCache, list: Partial<PersonObj>[] | Partial<ActivityObj>[]): void {
     if (Array.isArray(list)) {
       list.forEach(item => {
         if (key === 'resource' && item && item.id && !this.getCache(key).has(item.id)) {
-          this.getCache(key).set(item.id, { ...item, firstName: (item.firstName || ''), lastName: (item.lastName || '') })
+          this.getCache(key).set(item.id, { ...item, firstName: (item.firstName || ''), lastName: (item.lastName || '') });
         }
 
         if (key === 'activity' && item && item.id && !this.getCache(key).has(item.id)) {
-          this.getCache(key).set(item.id, { ...item })
+          this.getCache(key).set(item.id, { ...item });
         }
-      })
+      });
     }
   }
 
-  private _query<T_LIST extends T_ITEM[], T_ITEM extends {}>(query: string) {
+  // tslint:disable-next-line:max-line-length
+  private _query<T_LIST extends QueryResponse<T_ITEM>, T_ITEM extends {}>(query: string): Observable<Observable<T_LIST> extends ObservableInput<infer T> ? T : never> {
     // console.debug(query);
     return this.auth.globalContextWithAuth$.pipe(
-      mergeMap(ctx => this.http.post<T_LIST>(`${this.config.getApiUri()}/query`, { query }, { headers: this.getHeaders(ctx) })),
-
+      mergeMap(ctx => this.http.post<T_LIST>(`${this.config.getApiUri()}/query`, { query }, { headers: this.getHeaders(ctx) }))
     );
   }
 
-  public queryActivities<list extends item[], item extends ActivityObj>(query: string) {
-    return this._query<list, item>(query).pipe(
-      tap((list) => this.addToCache('activity', list))
-    )
+  public queryActivities<list extends item[],
+    item extends ActivityObj>(query: string): Observable<Observable<QueryResponse<item>> extends ObservableInput<infer T> ? T : never> {
+    return this._query<QueryResponse<item>, item>(query).pipe(
+      tap((currentList) => this.addToCache('activity', currentList.data))
+    );
   }
 
 
-  public queryResource<list extends item[], item extends { id: string, firstName: string, lastName: string }>(query: string) {
+  public queryResource<list extends QueryResponse<item>, item extends {
+    id: string,
+    firstName: string,
+    lastName: string
+  }>(query: string): Observable<any> {
     return combineLatest([
       this._query<list, item>(query),
-      this._query<TagObj[], TagObj>(`SELECT tag.id as id, tag.name as name FROM Tag tag WHERE tag.inactive = false`),
-      this._query<SkillObj[], SkillObj>(`SELECT skill.id as id, skill.tag as tag, skill.person as person, skill.startDate as startDate, skill.endDate as endDate FROM Skill skill WHERE skill.inactive = false`)
+      this._query<QueryResponse<TagObj>, TagObj>(`SELECT tag.id as id, tag.name as name
+                                     FROM Tag tag
+                                     WHERE tag.inactive = false`),
+      this._query<QueryResponse<SkillObj>, SkillObj>(`SELECT skill.id        as id,
+                                                skill.tag       as tag,
+                                                skill.person    as person,
+                                                skill.startDate as startDate,
+                                                skill.endDate   as endDate
+                                         FROM Skill skill
+                                         WHERE skill.inactive = false`)
     ])
       .pipe(
         map(([resources, tags, skills]) => {
-          const tagMap = tags.reduce((m, { id, name }) => m.set(id, name), new Map<string, string>())
-          const skillMap = skills.reduce((m, skill) => {
-            const item = {
+          const tagMap = tags.data.reduce((m, { id, name }) => m.set(id, name), new Map<string, string>());
+          const skillMap = skills.data.reduce((m, skill) => {
+            const currentItem = {
               name: (tagMap.get(skill.tag) || skill.tag),
-              start: skill.startDate === "null" ? null : skill.startDate,
-              end: skill.endDate === "null" ? null : skill.endDate,
+              start: skill.startDate === 'null' ? null : skill.startDate,
+              end: skill.endDate === 'null' ? null : skill.endDate
             };
             if (m.has(skill.person)) {
-              m.get(skill.person)?.push(item)
+              m.get(skill.person)?.push(currentItem);
             } else {
-              m.set(skill.person, [item])
+              m.set(skill.person, [currentItem]);
             }
             return m;
-          }, new Map<string, { name: string, start: string, end: string }[]>())
+          }, new Map<string, { name: string, start: string, end: string }[]>());
 
-          return resources.map(it => ({ ...it, skills: skillMap.has(it.id) ? skillMap.get(it.id).sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)) : [] }))
+          return resources.data.map(it => ({
+            ...it,
+            skills: skillMap.has(it.id) ? skillMap.get(it.id).sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)) : []
+          }));
         }),
-        tap((list) => this.addToCache('resource', list))
+        tap((currentList) => this.addToCache('resource', currentList))
       );
   }
 
@@ -115,28 +160,5 @@ export class QueryService {
     return this.getCache('resource').has(id)
       ? this.getCache('resource').get(id)
       : { id, firstName: 'N/A', lastName: 'N/A', skills: [] };
-  }
-
-  public static toLastChangedDateStringFormat = (lastChanged: number): string => {
-    // ¯\_(ツ)_/¯ queryApi only supports only dateTime on [lastChanged]
-    // but only formats suppored are:
-    // [yyyy-MM-ddThh:mm:ssZ]
-    // [yyyy-MM-dd hh:mm:ss.SSS]
-    // ISO:
-    // [yyyy-MM-ddThh:mm:ss.SSSZ] is NOT suppored
-    const lastChangedDateString = new Date(lastChanged)
-      .toISOString()
-      .replace('T', ' ')
-      .replace('Z', '');
-
-    return lastChangedDateString;
-  };
-
-  public static trimMilliseconds = (dateString: string) => {
-    // ¯\_(ツ)_/¯ queryApi and dates ...
-    // ISO: [yyyy-MM-ddThh:mm:ss.SSSZ] is NOT suppored
-    // but 
-    // [yyyy-MM-ddThh:mm:ssZ] 
-    return new Date(new Date(dateString).setMilliseconds(0)).toISOString().replace('.000', '');
   }
 }
