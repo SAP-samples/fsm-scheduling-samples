b745788be2247468e80233f04d623d745b4209da
Davo00 <davit.melkonyan@sap.com>
2023-11-15 10:24:27 +0100

    listening to selection and filtering resources based on multi-selection in skills dropdown

--- workbench/frontend/src/app/common/components/resource-query/resource-query.component.ts
+++ workbench/frontend/src/app/common/components/resource-query/resource-query.component.ts
@@ -8,29 +8,29 @@ import { QueryService } from '../../services/query.service';
 
 const TEMPLATES = {
   default: `
-SELECT 
+SELECT
   resource.id as id,
   resource.firstName as firstName,
   resource.lastName as lastName
-FROM 
-  UnifiedPerson resource 
-WHERE 
-  resource.plannableResource = true 
+FROM
+  UnifiedPerson resource
+WHERE
+  resource.plannableResource = true
   AND resource.inactive != true
 LIMIT 25
 `,
   skill: `
-SELECT 
+SELECT
   resource.id as id,
   resource.firstName as firstName,
   resource.lastName as lastName
-FROM 
-  Tag tag 
+FROM
+  Tag tag
   LEFT JOIN Skill skill ON skill.tag = tag.id
   LEFT JOIN UnifiedPerson resource ON resource.id = skill.person
 WHERE
-  resource.plannableResource = true 
-  AND resource.inactive != true 
+  resource.plannableResource = true
+  AND resource.inactive != true
   AND tag.name = '<insert-skill-here>'
 LIMIT 25
 `,
@@ -39,16 +39,16 @@ SELECT
   resource.id as id,
   resource.firstName as firstName,
   resource.lastName as lastName
-FROM 
-  UnifiedPerson resource 
+FROM
+  UnifiedPerson resource
   LEFT JOIN Region region ON region.name = '<insert-region-here>'
-WHERE 
+WHERE
   region.id IN resource.regions
-  AND resource.plannableResource = true 
-  AND resource.inactive != true 
+  AND resource.plannableResource = true
+  AND resource.inactive != true
 LIMIT 25
 `
-}
+};
 
 @Component({
   selector: 'resource-query',
@@ -61,10 +61,16 @@ export class ResourceQueryComponent implements OnInit, OnDestroy {
   public resources$ = new BehaviorSubject<Partial<{ id: string; firstName: string, lastName: string }>[]>([]);
   public isLoading$ = new BehaviorSubject<boolean>(false);
 
+  public allSkills = [];
+  public skillResourcesMap: Map<string, any[]> = new Map();
+  public allResources: any[] = [];
+  public selectedSkills: string[] = [];
+
+
   @Output() change = new EventEmitter<string[]>();
 
   public form: FormGroup;
-  private onDistroy$ = new Subject();
+  private onDestroy$ = new Subject();
   public editorOptions = {
     theme: 'vs-dark',
     language: 'pgsql',
@@ -77,23 +83,40 @@ export class ResourceQueryComponent implements OnInit, OnDestroy {
     private snackBar: MatSnackBar,
   ) { }
 
-  public ngOnDestroy() {
-    this.onDistroy$.next();
+  public ngOnDestroy(): void {
+    this.onDestroy$.next();
   }
 
   public ngOnInit(): void {
     this.form = this.fb.group({
       query: [TEMPLATES.default],
+      selectedSkills: []
     });
 
-    this.resources$.pipe(
-      tap(list => {
-        if (list.length) {
-          this.change.next(list.map(it => it.id));
-        }
-      }),
-      takeUntil(this.onDistroy$)
-    ).subscribe();
+    this.svc.queryResource(TEMPLATES.default).subscribe(resources => {
+      this.allResources = resources;
+      this.skillResourcesMap.clear();
+
+      resources.forEach(resource => {
+        resource.skills.forEach(skill => {
+          const key = skill.name.toLowerCase();
+          if (!this.skillResourcesMap.has(key)) {
+            this.skillResourcesMap.set(key, []);
+          }
+          this.skillResourcesMap.get(key).push(resource);
+        });
+      });
+
+      this.resources$.next(this.allResources);
+      this.allSkills = Array.from(this.skillResourcesMap.keys());
+    });
+
+    this.form.get('selectedSkills').valueChanges.pipe(
+      takeUntil(this.onDestroy$)
+    ).subscribe(selectedSkills => {
+      this.selectedSkills = selectedSkills;
+      this.updateResources();
+    });
 
     this.onQuery.pipe(
       withLatestFrom(merge(of(this.form.value), this.form.valueChanges)),
@@ -107,40 +130,55 @@ export class ResourceQueryComponent implements OnInit, OnDestroy {
             if (error instanceof HttpErrorResponse) {
               errorMessage = `Error [❌ ${error.status} ❌ ]\n\n${error.message}`;
             }
-
-            const snackBarRef = this.snackBar.open(errorMessage, 'ok', { duration: 3000 });
-
+            this.snackBar.open(errorMessage, 'ok', { duration: 3000 });
             return of([]);
           })
-        )
+        );
       }),
       tap(list => {
         this.isLoading$.next(false);
         this.resources$.next(list);
       }),
-      takeUntil(this.onDistroy$)
+      takeUntil(this.onDestroy$)
     ).subscribe();
 
+    // Trigger the initial query after a delay
     setTimeout(() => this.onQuery.next(), 100);
   }
 
-  public onEditor(editor) {
+  public onEditor(editor): void {
     // let line = editor.getPosition();
   }
 
-  public remove(item: { id: string }) {
+  public remove(item: Partial<{ id: string; firstName: string; lastName: string }>): void {
     this.resources$.pipe(
       take(1),
       tap(current => this.resources$.next(current.filter(it => it.id !== item.id)))
     ).subscribe();
   }
 
-  public doQuery() {
+  public doQuery(): void {
     this.onQuery.next();
   }
 
-  public applyTmpl(t: keyof typeof TEMPLATES) {
+  public applyTmpl(t: keyof typeof TEMPLATES): void {
     this.form.patchValue({ query: TEMPLATES[t] });
+  }
+
+  private updateResources(): void {
+    if (this.selectedSkills.length > 0) {
+      const filteredResourcesSet = new Set<any>();
+
+      this.selectedSkills.forEach(skill => {
+        const resourcesForSkill = this.skillResourcesMap.get(skill.toLowerCase()) || [];
+        resourcesForSkill.forEach(resource => filteredResourcesSet.add(resource));
+      });
+
+      this.resources$.next(Array.from(filteredResourcesSet));
+      console.log(this.resources$);
+    } else {
+      this.resources$.next(this.allResources);
+    }
   }
 
 }
